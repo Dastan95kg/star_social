cc55e50096df6a4cbbf72cd63b5c01679844f19f
dastan <>
2019-10-22 01:03:22 +0600

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from . import forms
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
